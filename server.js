d71fc7d303517fd478f428045d0452c43487cb03
malli7 <51438017+malli7@users.noreply.github.com>
2022-12-22 11:39:51 +0530

    api key commit

--- server.js
+++ server.js
@@ -1,36 +1,24 @@
 // Require the express module
-const express = require('express');
+const express = require("express");
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // Require the cors module, which allows for Cross-Origin Resource Sharing (CORS)
-var cors = require('cors')
+var cors = require("cors");
 
 // Create an instance of the express app
 const app = express();
 
 // Use the express.json() middleware to parse JSON in the request body
-app.use(express.json())
+app.use(express.json());
 
 // Use the cors middleware to allow for CORS requests
-app.use(cors())
+app.use(cors());
 
 // Use the /api/ route for all requests to the weather router
-app.use('/api/', require('./routes/weather'))
-
-
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static('client/build'))
-    const path = require('path')
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-    })
-
-}
-
-
+app.use("/api/", require("./routes/weather"));
 
 // Start the server and listen on port 5000
 app.listen(PORT, () => {
-console.log('Server listening on port', PORT);
+  console.log("Server listening on port", PORT);
 });
